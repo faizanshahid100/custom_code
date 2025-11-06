efa7083cd6fb6840e08845971ddc9579e847930f
Farooq Butt <farooq.butt@primesystemsolutions.com>
2025-11-06 17:02:20 +0500

    [Asset Management]

--- asset_management_system/models/asset.py
+++ asset_management_system/models/asset.py
@@ -1,6 +1,8 @@
 from email.policy import default
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class AssetManagementAsset(models.Model):
     _name = 'asset.management.asset'
@@ -9,6 +11,12 @@ class AssetManagementAsset(models.Model):
     _rec_name = 'display_name'
 
     name = fields.Char(string="Asset Name", required=True, tracking=True)
+    state = fields.Selection([
+        ('in_store', 'In Store'),
+        ('in_running', 'In Running'),
+        ('in_repair', 'In Repair'),
+        ('scraped', 'Scraped'),
+    ], string="Status", default='in_store', tracking=True)
     sequence_no = fields.Char(string='Asset No', required=True, copy=False, readonly=True, default='New', tracking=True)
     serial_no = fields.Char(string="Serial No.", required=True, tracking=True)
     model_name = fields.Char(string="Model", required=True, tracking=True)
@@ -18,6 +26,8 @@ class AssetManagementAsset(models.Model):
     condition_rating = fields.Integer(string="Equipment Condition (1-10)", default=10, required=True, tracking=True)
     reason_below_8 = fields.Text(string="Reason (if below 8)", readonly=False, tracking=True)
     is_allotted = fields.Boolean('Is Allotted', default=False, tracking=True)
+    is_in_repair = fields.Boolean('In Repair', default=False, tracking=True)
+    is_in_scrap = fields.Boolean('In Scrap', default=False, tracking=True)
     assigned_id = fields.Many2one('asset.management.assignment', string='Assigned To')
 
     display_name = fields.Char(compute='_compute_display_name', store=True)
@@ -41,3 +51,16 @@ class AssetManagementAsset(models.Model):
             vals['sequence_no'] = self.env['ir.sequence'].next_by_code('asset.management.asset') or 'New'
         return super(AssetManagementAsset, self).create(vals)
 
+    def action_set_in_store(self):
+        self.write({'state': 'in_store', 'is_allotted': False,'is_in_repair': False, 'is_in_scrap':False, 'assigned_id':False})
+    def action_set_in_repair(self):
+        if self.state == 'in_store':
+            self.write({'state': 'in_repair', 'is_in_repair': True })
+        else:
+            raise ValidationError('Please move the asset to the Store first.')
+
+    def action_set_scraped(self):
+        if self.state == 'in_store':
+            self.write({'state': 'scraped', 'is_in_scrap': True})
+        else:
+            raise ValidationError('Please move the asset to the Store first.')
--- asset_management_system/models/assignment.py
+++ asset_management_system/models/assignment.py
@@ -17,8 +17,6 @@ class AssetManagementAssignment(models.Model):
     state = fields.Selection([
         ('in_store', 'In Store'),
         ('in_running', 'In Running'),
-        ('in_repair', 'In Repair'),
-        ('scraped', 'Scraped'),
     ], string="Status", default='in_store', tracking=True)
     asset_line_ids = fields.One2many('asset.management.assignment.line', 'assignment_id', string="Assigned Assets")
 
@@ -34,51 +32,53 @@ class AssetManagementAssignment(models.Model):
         employee = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
         return employee.id if employee else False
 
+    def _validate_duplicate_assets(self):
+        """Raise error if same asset appears multiple times."""
+        for rec in self:
+            asset_ids = rec.asset_line_ids.mapped('asset_id.id')
+            if len(rec.asset_line_ids) != len(set(asset_ids)):
+                raise ValidationError(
+                    "Duplicate assets detected in assignment lines. Each asset can only be assigned once.")
+
     # -------------------------------------
     # 🔹 State change actions with validation
     # -------------------------------------
-    def _validate_state_change(self):
+    def _is_already_allotted(self):
         """Prevent skipping 'in_store' when asset already allotted."""
         for rec in self.asset_line_ids:
-            allotted_assets = rec.asset_id.is_allotted
-            if allotted_assets and rec.state == 'in_store':
+            if rec.asset_id.is_allotted:
                 raise ValidationError(
                     f"Assets {rec.asset_id.name} {rec.asset_id.serial_no} already allotted. Please set state to 'In Store' first before changing it to another state."
                 )
 
-    # -----------------------------
-    # 🔹 State change button actions
-    # -----------------------------
-    def _update_asset_allocation(self):
-        """Update the asset's is_allotted field based on assignment state."""
-        for rec in self:
-            is_allotted = rec.state != 'in_store'
-            rec.asset_line_ids.mapped('asset_id').write({'is_allotted': is_allotted})
-
     def update_assets_allotment(self):
         for rec in self.asset_line_ids:
             if self.state == 'in_store':
-                rec.asset_id.write({'is_allotted': True})
+                rec.asset_id.write({'is_allotted': True, 'assigned_id':self.id, 'state':'in_running'})
             elif self.state != 'in_store':
-                rec.asset_id.write({'is_allotted': False})
+                rec.asset_id.write({'is_allotted': False, 'assigned_id':False, 'state': 'in_store'})
+    # -----------------------------
+    # 🔹 State change button actions
+    # -----------------------------
+    # def _update_asset_allocation(self):
+    #     """Update the asset's is_allotted field based on assignment state."""
+    #     for rec in self:
+    #         is_allotted = rec.state != 'in_store'
+    #         if rec.state != 'in_store':
+    #             rec.asset_line_ids.mapped('asset_id').write({'is_allotted': is_allotted, 'assigned_id':self.id})
+    #         elif rec.state == 'in_store':
+    #             rec.asset_line_ids.mapped('asset_id').write({'is_allotted': False, 'assigned_id': False})
+
 
     def action_set_in_store(self):
         self.update_assets_allotment()
         self.state = 'in_store'
-        self._update_asset_allocation()
 
     def action_set_in_running(self):
+        self._validate_duplicate_assets()
+        self._is_already_allotted()
         self.update_assets_allotment()
         self.state = 'in_running'
-        self._update_asset_allocation()
-
-    def action_set_in_repair(self):
-        self.state = 'in_repair'
-        self._update_asset_allocation()
-
-    def action_set_scraped(self):
-        self.state = 'scraped'
-        self._update_asset_allocation()
 
 
 class AssetManagementAssignmentLine(models.Model):
@@ -98,29 +98,29 @@ class AssetManagementAssignmentLine(models.Model):
     # -----------------------------------------
     # 🔹 Onchange - toggle is_allotted on select/unselect
     # -----------------------------------------
-    @api.onchange('asset_id')
-    def _onchange_asset_id(self):
-        for rec in self:
-            # if old asset existed → make it unallotted
-            if rec._origin and rec._origin.asset_id and rec._origin.asset_id != rec.asset_id:
-                rec._origin.asset_id.is_allotted = False
-            # if new asset selected → mark allotted
-            if rec.asset_id:
-                rec.asset_id.is_allotted = True
+    # @api.onchange('asset_id')
+    # def _onchange_asset_id(self):
+    #     for rec in self:
+    #         # if old asset existed → make it unallotted
+    #         if rec._origin and rec._origin.asset_id and rec._origin.asset_id != rec.asset_id:
+    #             rec._origin.asset_id.is_allotted = False
+    #         # if new asset selected → mark allotted
+    #         if rec.asset_id:
+    #             rec.asset_id.is_allotted = True
 
     # -----------------------------------------
     # 🔹 Ensure data consistency at DB level
     # -----------------------------------------
-    def write(self, vals):
-        for rec in self:
-            # If asset is being changed, unallot old one
-            if 'asset_id' in vals and rec.asset_id:
-                rec.asset_id.write({'is_allotted': False})
-        res = super().write(vals)
-        # Mark new asset as allotted
-        if 'asset_id' in vals:
-            self.mapped('asset_id').write({'is_allotted': True})
-        return res
+    # def write(self, vals):
+    #     for rec in self:
+    #         # If asset is being changed, unallot old one
+    #         if 'asset_id' in vals and rec.asset_id:
+    #             rec.asset_id.write({'is_allotted': False})
+    #     res = super().write(vals)
+    #     # Mark new asset as allotted
+    #     if 'asset_id' in vals:
+    #         self.mapped('asset_id').write({'is_allotted': True})
+    #     return res
 
     def unlink(self):
         """When line deleted, unmark the asset."""
